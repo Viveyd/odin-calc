c8b7f72a48092cc28e1a87c028cecac6004ef677
vivi <harhar.asuncion@gmail.com>
2021-07-11 01:28:24 +0800

    Bugfix to .js file. Cleaner code but stil buggy.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function getProduct(num1, num2){
 function getQuotient(num1, num2){
     return num1 / num2;
 }
-function getResult(num1, operator, num2){
+function operate(num1, operator, num2){
     switch(operator){
         case "+":
             return getSum(num1, num2);
@@ -23,87 +23,100 @@ function getResult(num1, operator, num2){
     }
 }
 function registerInput(e){
-    if(overwriteNext){
-        ioDisplay.textContent = "";
-        overwriteNext= false;
-        
-    }
-    if(e.target.textContent == "."){
-        e.target.disabled = true;
-    }
-    if(e.target.classList.contains('numerals')) ioDisplay.textContent += e.target.textContent; 
-    else if(e.target.classList.contains('operator')){
-        if(e.target.textContent == "=" && ioDisplay.textContent != "" && dataStore['operand1'] != null && dataStore['operator'] != null){
-            dataStore['operand2'] = Number(ioDisplay.textContent);
-            displayResult();
-            
-        }
-        else if(e.target.textContent != "=" ){
-            if(dataStore['operand1'] != null && dataStore['operator'] != null){
-                dataStore['operand2'] = Number(ioDisplay.textContent);
-                displayResult();
-                dataStore['operator'] = e.target.textContent;
-            }
-            else{
-                dataStore['operand1'] = Number(ioDisplay.textContent);
-                dataStore['operator'] = e.target.textContent;
-                ioDisplay.textContent = "";
-            }
-            
-        }
-    }
-    else if(e.target.classList.contains('util')){
+
+    lastOutput = ioDisplay.textContent;
+    if(e.target.classList.contains('util')){
         if(e.target.getAttribute('id') == 'clear-btn'){
             dataStore = {operand1: null, operator: null, operand2: null};
             ioDisplay.textContent = "";
         }
-        
         else{
             let ioArray = ioDisplay.textContent.split('');
             ioArray.pop();
             ioDisplay.textContent = ioArray.join('');
         } 
     }
-}
-
-function displayResult(){
-    // let operand1 = dataStore['operand1'];
-    if(dataStore['operand2'] == 0){
-        ioDisplay.textContent = "Err0r";
-        dataStore['operand1'] = null;
-        dataStore['operator'] = null;
-        dataStore['operand2'] = null;
-        overwriteNext = true;
-    } 
     else{
-        let result = getResult(dataStore['operand1'], dataStore['operator'], dataStore['operand2']);
-        console.log(`Operation is ${dataStore['operand1']} ${dataStore['operator']} ${dataStore['operand2']}`);
-        dataStore['operand1'] = result;
-        dataStore['operand2'] = null;
-        ioDisplay.textContent = result;
-        overwriteNext = true;
+        if(e.target.classList.contains('numerals')){
+            if(ioDisplay.getAttribute('showing-result') == 'true'){
+                ioDisplay.setAttribute('showing-result', 'false');
+                dataStore['operand1'] = null;
+                dataStore['operand2'] = null;
+            }
+            if(overwrite){
+                displayOutput(e.target.textContent, overwrite);
+                overwrite = false;
+            }
+            else{
+                displayOutput(e.target.textContent);
+            }
+        }
+        else if(e.target.classList.contains('operator')){
+            if(ioDisplay.textContent == ""){ 
+                return 0 ;
+
+            }
+
+            if(ioDisplay.getAttribute('showing-result') == 'true' && e.target.getAttribute('id') != "equals-btn" ){
+                dataStore['operand1'] = ioDisplay.textContent;
+                dataStore['operator'] = e.target.textContent;
+                overwrite = true;
+                ioDisplay.setAttribute('showing-result', 'false');
+            }
+            else{
+                if((dataStore['operand1'] == null && dataStore['operand2'] == null)){
+                    dataStore['operand1'] = ioDisplay.textContent;
+                    overwrite = true;
+                   
+                } 
+                // else if(dataStore['operand1'] != null && dataStore['operator'] != null && dataStore['operand2'] == null){
+                //     dataStore['operand2'] = ioDisplay.textContent;
+                //     overwrite = true;
+                // }
+
+            }
+            // if((dataStore['operand1'] == null && dataStore['operand2'] == null)){
+                
+
+            // }
+            // else if(dataStore['operand1'] != null && dataStore['operand2'] == null) {
+            //     dataStore['operand2'] = ioDisplay.textContent;
+            //     ioDisplay.textContent = "";
+            // };
+            if(e.target.getAttribute('id') == "equals-btn" || (dataStore['operand1'] != null && dataStore['operator'] != null )){
+                if(dataStore['operand1']!= null && dataStore['operator']!= null ){
+                    dataStore['operand2'] = ioDisplay.textContent;
+                    let output = operate(Number(dataStore['operand1']), dataStore['operator'], Number(dataStore['operand2']));
+                    console.log(output);
+                    overwrite = true;
+                    displayOutput(output, overwrite);
+                    dataStore['operand1'] = ioDisplay.textContent;
+                    // dataStore['operator'] = null;
+                    dataStore['operand2'] = null;
+                    ioDisplay.setAttribute('showing-result', 'true');
+                }
+            }
+            else{
+                dataStore['operator'] = e.target.textContent;
+            }
+            lastTarget = e.target;
+        }
     }
-
-
 }
 
-// document.addEventListener('keydown', (e) => {
-//     if (e.key != "F5")e.preventDefault();
-// })
-// document.addEventListener('keyup', (e) => {
-    
-// })
-let overwriteNext = false;
+function displayOutput(output, overwrite){
+    if(overwrite) ioDisplay.textContent = output;
+    else ioDisplay.textContent += output;
+}
+
+let lastTarget;
+let prepOperand2;
+let overwrite= false;
 let matcher = ["/", "*", "-", "+", "Enter", "Del"];
 let point = document.getElementById('point-btn');
 let dataStore = {operand1: null, operator: null, operand2: null};
 let ioDisplay = document.getElementById('io-display');
 let main = document.getElementsByTagName('main')[0];
 main.addEventListener('click', (e) => registerInput(e));
-main.addEventListener('mouseup', () => {
-    if(ioDisplay.textContent.includes('.')){
-        point.disabled = true;
-    } else point.disabled = false;
-}
-)
+
 
